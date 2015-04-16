c05debf2421c75ddbb9eb755d899d6cbd17cf468
mattmb <matthew.mead-briggs@digital.justice.gov.uk>
2015-04-16 12:03:10 +0100

    Docker authentication
    Add ability to authenticate to docker by specifying pillar data with
    logins for each registry.

---
+++ moj-docker-deploy/apps/templates/docker_logins.py
@@ -0,0 +1,15 @@
+#!py
+# This is a template that uses salt's python renderer:
+# See http://docs.saltstack.com/en/latest/ref/states/all/salt.states.file.html
+# in this case it returns json which is the intended contents of the file.
+import base64
+import json
+
+def run():
+    docker_logins = salt['pillar.get']('registry_logins',{})
+    for reg, login in docker_logins.items():
+        user = login.pop('user')
+        password = login.pop('password')
+        auth = base64.b64encode("{0}:{1}".format(user,password))
+        docker_logins[reg]['auth'] = auth
+    return json.dumps(docker_logins)
